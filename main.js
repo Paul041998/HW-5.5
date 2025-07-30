5ee04b3d6305f0445cc5deb1cbfabf1ed820afdc
Paul041998 <amsterdampawloamsterdam@gmail.com>
2025-07-30 21:54:42 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let userNumber = parseInt(prompt("Введіть ціле число"));
+let power = 1;
+let numberToThePowerOfThree = false;
+
+for (let i = 0; i <= userNumber; i++) {
+  if (power === userNumber) {
+    alert((userNumber = "можна отримати як 3 у ступені " + i + "."));
+    numberToThePowerOfThree = true;
+    break;
+  }
+  power *= 3;
+}
+if (!numberToThePowerOfThree) {
+  alert(userNumber + " не можна отримати як 3 у ступені.");
+}
